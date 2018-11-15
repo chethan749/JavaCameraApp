4bb9a40090e4708fc1b7951dae9b9ff142330a87
chethan749 <chethan749@gmail.com>
2018-11-16 00:30:00 +0530

    Added Tracking

--- src/application/CameraAppFXController.java
+++ src/application/CameraAppFXController.java
@@ -1,14 +1,13 @@
 package application;
 
-import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import javafx.event.ActionEvent;
-import javafx.event.EventType;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
@@ -16,10 +15,8 @@ import javafx.scene.image.ImageView;
 import org.opencv.core.*;
 import org.opencv.core.Point;
 import org.opencv.imgproc.Imgproc;
-import org.opencv.video.SparsePyrLKOpticalFlow;
 import org.opencv.video.Video;
 import org.opencv.videoio.VideoCapture;
-
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -35,31 +32,46 @@ public class CameraAppFXController
     private ImageView trackerFrame;
     @FXML
     private Button trackerButton;
+    @FXML
+    private Button choosePointsButton;
+
     private ScheduledExecutorService cameraTimer;
     private ScheduledExecutorService faceTimer;
     private ScheduledExecutorService trackerTimer;
-    private VideoCapture capture = new VideoCapture();
-    private boolean blackAndWhite;
-    private boolean blur;
-    private boolean negative;
+    private ScheduledExecutorService pointsTimer;
+
+    private VideoCapture capture;
     private boolean cameraActive;
     private boolean trackerActive;
     private static int cameraId = 0;
     private Image imageToShow;
     private Mat frame;
+    private Utils utils;
+
+    private boolean blackAndWhite;
+    private boolean blur;
+    private boolean negative;
     private String base_file = "snaps/";
     private FaceDetector faceDetector;
     private MatOfRect faces;
-    private Utils utils;
+
+    private boolean readPoints;
+    private ArrayList<Point> points;
+    private MatOfPoint2f trackedPoints;
+    private Mat oldFrame;
 
     public CameraAppFXController()
     {
+        capture = new VideoCapture();
         cameraActive = false;
         blackAndWhite = false;
         blur = false;
         negative = false;
         faceDetector = new FaceDetector();
         utils = new Utils();
+        readPoints = false;
+        points = new ArrayList<>();
+        trackedPoints = new MatOfPoint2f();
     }
 
     @FXML
@@ -114,7 +126,7 @@ public class CameraAppFXController
                 };
 
                 this.cameraTimer = Executors.newSingleThreadScheduledExecutor();
-                this.cameraTimer.scheduleAtFixedRate(frameGrabber, 0L, 1L, TimeUnit.MILLISECONDS);
+                this.cameraTimer.scheduleAtFixedRate(frameGrabber, 0L, 30L, TimeUnit.MILLISECONDS);
                 this.faceTimer = Executors.newSingleThreadScheduledExecutor();
                 this.faceTimer.scheduleAtFixedRate(faceDetectorRunnable, 1000L, 1L, TimeUnit.MILLISECONDS);
 
@@ -140,6 +152,13 @@ public class CameraAppFXController
             this.stopCamera();
         }
 
+        if(!pointsTimer.isShutdown())
+        {
+            this.choosePointsButton.setText("Choose Points");
+            this.readPoints = false;
+            stopReadPoints();
+        }
+
         if (!this.trackerActive)
         {
             this.capture.open(cameraId);
@@ -154,14 +173,23 @@ public class CameraAppFXController
 
                         Mat frameCopy = frame.clone();
 
+                        Video.calcOpticalFlowPyrLK(oldFrame, frameCopy, trackedPoints, trackedPoints, new MatOfByte(), new MatOfFloat());
+
+                        Point[] pointsArray = trackedPoints.toArray();
+                        for(int i = 0; i < pointsArray.length; i++)
+                            Imgproc.circle(frameCopy, new Point(pointsArray[i].x, pointsArray[i].y), 5, new Scalar(0, 255, 0), -1);
+
                         imageToShow = Utils.mat2Image(frameCopy);
+                        oldFrame = frameCopy;
 
                         CameraAppFXController.this.trackerFrame.setImage(imageToShow);
                     }
                 };
 
+                oldFrame = CameraAppFXController.this.grabFrame();
+                trackedPoints.fromList(points);
                 this.trackerTimer = Executors.newSingleThreadScheduledExecutor();
-                this.trackerTimer.scheduleAtFixedRate(frameGrabber, 0L, 1L, TimeUnit.MILLISECONDS);
+                this.trackerTimer.scheduleAtFixedRate(frameGrabber, 0L, 30L, TimeUnit.MILLISECONDS);
 
                 this.trackerButton.setText("Stop Tracking");
             }
@@ -172,6 +200,7 @@ public class CameraAppFXController
         else {
             this.trackerActive = false;
             this.trackerButton.setText("Start Tracking");
+            points.clear();
             this.stopTracking();
         }
     }
@@ -198,14 +227,6 @@ public class CameraAppFXController
         blackAndWhite = false;
         blur = false;
         negative = true;
-    }
-
-    @FXML
-    protected void setFaceswap(ActionEvent event)
-    {
-        blackAndWhite = false;
-        blur = false;
-        negative = false;
     }
 
     @FXML
@@ -245,8 +266,64 @@ public class CameraAppFXController
     }
 
     @FXML
-    protected void pushPoint(ActionEvent event)
+    protected void readPoints(ActionEvent event)
     {
+        if(this.trackerActive)
+        {
+            this.trackerActive = false;
+            this.trackerButton.setText("Start Tracking");
+            this.stopTracking();
+        }
+
+        if(this.cameraActive)
+        {
+            this.cameraActive = false;
+            this.cameraButton.setText("Start Camera");
+            this.stopCamera();
+        }
+
+        this.capture.open(cameraId);
+        points.clear();
+        trackedPoints.release();
+
+        Runnable frameGrabber = new Runnable()
+        {
+            public void run()
+            {
+                if(!capture.isOpened())
+                    return;
+                frame = CameraAppFXController.this.grabFrame();
+
+                trackedPoints.fromList(CameraAppFXController.this.points);
+
+                Point[] pointsArray = trackedPoints.toArray();
+                for(int i = 0; i < pointsArray.length; i++)
+                    Imgproc.circle(frame, new Point(pointsArray[i].x, pointsArray[i].y), 5, new Scalar(0, 255, 0), -1);
+                imageToShow = Utils.mat2Image(frame);
+
+                CameraAppFXController.this.trackerFrame.setImage(imageToShow);
+            }
+        };
+
+        if(!readPoints)
+        {
+            readPoints = true;
+            choosePointsButton.setText("Done");
+            this.pointsTimer = Executors.newSingleThreadScheduledExecutor();
+            this.pointsTimer.scheduleAtFixedRate(frameGrabber, 0L, 30L, TimeUnit.MILLISECONDS);
+        }
+        else
+        {
+            readPoints = false;
+            choosePointsButton.setText("Choose Points");
+            stopReadPoints();
+        }
+
+        trackerFrame.setOnMouseClicked(e -> {
+            if(!readPoints)
+                return;
+            points.add(new Point(e.getX(), e.getY()));
+        });
     }
 
     private Mat grabFrame() {
@@ -331,7 +408,28 @@ public class CameraAppFXController
         {
             this.capture.release();
         }
+    }
 
+    private void stopReadPoints()
+    {
+        if (this.pointsTimer != null && !this.pointsTimer.isShutdown())
+        {
+            try
+            {
+                this.pointsTimer.shutdown();
+                this.pointsTimer.awaitTermination(33L, TimeUnit.MILLISECONDS);
+                this.trackerFrame.setImage(null);
+            }
+            catch (InterruptedException var2)
+            {
+                System.err.println(var2);
+            }
+        }
+
+        if (this.capture.isOpened())
+        {
+            this.capture.release();
+        }
     }
 
     protected void setClosed() {
